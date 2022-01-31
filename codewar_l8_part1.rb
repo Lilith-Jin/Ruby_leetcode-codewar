eef4d083f0f1229ca2ab9e84e5e255426b363fac
Lilith <li6253462@gmail.com>
2022-01-31 14:08:21 +0800

    question8

--- codewar_l8_part1.rb
+++ codewar_l8_part1.rb
@@ -9,14 +9,23 @@
 
 # 7
 # 找出離某個數字最近的平方數
-def nearest_sq(n)
-    # 實作內容
-    # .round 四捨五入至整數
-    puts Math.sqrt(n).round ** 2
-  end
+# def nearest_sq(n)
+#     # 實作內容
+#     # .round 四捨五入至整數
+#     puts Math.sqrt(n).round ** 2
+#   end
   
-  puts nearest_sq(1)    # 1
-  puts nearest_sq(2)    # 1
-  puts nearest_sq(10)   # 9
-  puts nearest_sq(111)  # 121
-  puts nearest_sq(9999) # 10000
+#   puts nearest_sq(1)    # 1
+#   puts nearest_sq(2)    # 1
+#   puts nearest_sq(10)   # 9
+#   puts nearest_sq(111)  # 121
+#   puts nearest_sq(9999) # 10000
+
+def count_sheep(num)
+  # 實作內容
+  (1..num).to_a.map{|x| "#{x} sheep..."}.join("")
+end
+
+puts count_sheep(1)  # 印出 1 sheep...
+puts count_sheep(2)  # 印出 1 sheep...2 sheep...
+puts count_sheep(3)  # 印出 1 sheep...2 sheep...3 sheep...
