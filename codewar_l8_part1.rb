e5d65b04f5017fb771f97bcccb8af56f45a57213
Lilith <li6253462@gmail.com>
2022-02-18 13:45:51 +0800

    question 16

--- codewar_l8_part1.rb
+++ codewar_l8_part1.rb
@@ -106,19 +106,42 @@
 # puts correct("BUDAPE5T")   # BUDAPEST
 # puts correct("PAR15")      # PARIS
 
-# 15
-def count_sheeps(array)
-  # 實作內容
-  # array.select{|x|x == true}.count
-  array.count(true)
+# 15算算裡面有幾個 true
+# def count_sheeps(array)
+#   # 實作內容
+#   # array.select{|x|x == true}.count
+#   array.count(true)
 
+# end
+
+# array1 = [true,  true,  true,  false,
+#           true,  true,  true,  true ,
+#           true,  false, true,  false,
+#           true,  false, false, true ,
+#           true,  true,  true,  true ,
+#           false, false, true,  true ]
+              
+# puts count_sheeps(array1)  # 17
+
+# 16扣除陣列中最大值跟最小值之後的總和
+def sum_array(arr)
+  # 實作內容
+    array = Array(arr)
+    # Array將所有東西建立成array，即使是nil也會建立成[]
+    if array.count > 2
+      array.sort[1..-2].sum
+    else
+      0
+    end
 end
 
-array1 = [true,  true,  true,  false,
-          true,  true,  true,  true ,
-          true,  false, true,  false,
-          true,  false, false, true ,
-          true,  true,  true,  true ,
-          false, false, true,  true ]
-              
-puts count_sheeps(array1)  # 17
+puts sum_array(nil)                      # 0
+puts sum_array([])                       # 0
+puts sum_array([3])                      # 0
+puts sum_array([-3])                     # 0
+puts sum_array([ 3, 5])                  # 0
+puts sum_array([-3, -5])                 # 0
+puts sum_array([6, 2, 1, 8, 10])         # 16
+puts sum_array([6, 0, 1, 10, 10])        # 17
+puts sum_array([-6, -20, -1, -10, -12])  # -28
+puts sum_array([-6, 20, -1, 10, -12])    # 3
