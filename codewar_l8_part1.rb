f7627ca7eb6237a48686fadd3d4e53657bcb75a3
Lilith <li6253462@gmail.com>
2022-02-18 19:38:36 +0800

    question19

--- codewar_l8_part1.rb
+++ codewar_l8_part1.rb
@@ -157,13 +157,30 @@
 # p merge_arrays([2, 4, 8], [2, 4, 6])  # [2, 4, 6, 8]
 
 # 18. 縮寫產生器
-def abbrev_name(name)
+# def abbrev_name(name)
+#   # 實作內容
+#   name.split.map{|n| n[0].capitalize}.join(".")
+# end
+
+# puts abbrev_name("Sam Harris")      # S.H
+# puts abbrev_name("Patrick Feenan")  # P.F
+# puts abbrev_name("Evan Cole")       # E.C
+# puts abbrev_name("P Favuzzi")       # P.F
+# puts abbrev_name("David Mendieta")  # D.M
+
+# 19. 觀察輸入內容及輸出結果，完成實作內容
+
+def square_or_square_root(array)
   # 實作內容
-  name.split.map{|n| n[0].capitalize}.join(".")
+  array.map{ |n|
+    if Math.sqrt(n)%1 == 0
+      Integer.sqrt(n)
+    else
+      n**2
+    end
+  }
 end
 
-puts abbrev_name("Sam Harris")      # S.H
-puts abbrev_name("Patrick Feenan")  # P.F
-puts abbrev_name("Evan Cole")       # E.C
-puts abbrev_name("P Favuzzi")       # P.F
-puts abbrev_name("David Mendieta")  # D.M
+p square_or_square_root([4, 3, 9, 7, 2, 1])      # [2, 9, 3, 49, 4, 1]
+p square_or_square_root([100, 101, 5, 5, 1, 1])  # [10, 10201, 25, 25, 1, 1]
+p square_or_square_root([1, 2, 3, 4, 5, 6])      # [1, 4, 9, 2, 25, 36]
