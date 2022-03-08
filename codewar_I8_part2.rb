12ab237db3625b13c00ecb25d3da1020c3efb6df
Lilith <li6253462@gmail.com>
2022-03-08 22:41:27 +0800

    question21

--- codewar_I8_part2.rb
+++ codewar_I8_part2.rb
@@ -79,22 +79,32 @@
 
 # 20. correct 參數是正確答案，answer 參數是學生作答答案，每答對一題得 4 分，答錯一題扣 1 分，沒答不得分也不扣分，請完成實作內容：
 
-def check_exam(correct, answer)
-  # 實作內容
-  score = correct.zip(answer).map do|c,a|
-    case a
-    when c
-      4
-    when ""
-      0
-    else
-      -1
-    end
-  end.sum
-   (score > 0)? score : 0
-end
+# def check_exam(correct, answer)
+#   # 實作內容
+#   score = correct.zip(answer).map do|c,a|
+#     case a
+#     when c
+#       4
+#     when ""
+#       0
+#     else
+#       -1
+#     end
+#   end.sum
+#    (score > 0)? score : 0
+# end
 
-puts check_exam(["a", "a", "b", "b"], ["a", "c", "b", "d"])  # 6
-puts check_exam(["a", "a", "c", "b"], ["a", "a", "b",  ""])  # 7
-puts check_exam(["a", "a", "b", "c"], ["a", "a", "b", "c"])  # 16
-puts check_exam(["b", "c", "b", "a"], ["",  "a", "a", "c"])  # 0
+# puts check_exam(["a", "a", "b", "b"], ["a", "c", "b", "d"])  # 6
+# puts check_exam(["a", "a", "c", "b"], ["a", "a", "b",  ""])  # 7
+# puts check_exam(["a", "a", "b", "c"], ["a", "a", "b", "c"])  # 16
+# puts check_exam(["b", "c", "b", "a"], ["",  "a", "a", "c"])  # 0
+
+# 21. 請完成實作內容，請注意，在實作內容中不能使用 0123456789*+-/ 字元
+
+def give_me_five
+  # 實作內容
+  # Math.sqrt(25).to_i
+  "loveu".length
+end  
+
+puts give_me_five  # 5
