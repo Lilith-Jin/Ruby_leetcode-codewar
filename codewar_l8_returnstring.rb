dc47687533d86339c2f3b027934f65aea23f593d
Lilith <li6253462@gmail.com>
2022-03-15 16:20:55 +0800

    create method to return input string

---
+++ codewar_l8_returnstring.rb
@@ -0,0 +1,7 @@
+def greet(name)
+  #your code here
+  "Hello, #{name} how are you doing today?"
+end
+
+puts greet("Ryan") # "Hello, Ryan how are you doing today?"
+puts greet("Shingles")# "Hello, Shingles how are you doing today?"
