a341c78d21ab4eac0a3dc09904ed1316449c2383
Lilith <li6253462@gmail.com>
2022-03-24 23:01:33 +0800

    create method to split the bill

--- codewar_l7_split_the_bill.rb
+++ codewar_l7_split_the_bill.rb
@@ -1,11 +1,9 @@
 # 一夥人去吃飯，大家先各自付帳，待後續再計算多退少補，請根據輸出結果完成以下實作
 def split_the_bill(bill)
   # 實作內容
-  average = (bill.values.sum)/3
-  bill.values.each do|b|
-   puts (b - average).round(2) 
-  end
+  average = bill.values.sum / bill.count.to_f
+  bill.transform_values{|v|v - average}
 end
 
 p split_the_bill('A' => 20, 'B' => 15, 'C' => 10)#{'A' => 5.00, 'B' => 0.00, 'C' => -5.00}
-# p split_the_bill('A' => 40, 'B' => 25, 'X' => 10)#({'A' => 15.00, 'B' => 0.00, 'X' => -15.00})
+p split_the_bill('A' => 40, 'B' => 25, 'X' => 10)#({'A' => 15.00, 'B' => 0.00, 'X' => -15.00})
