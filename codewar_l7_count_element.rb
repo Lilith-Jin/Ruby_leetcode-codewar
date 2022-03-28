d849fd04b045df783b81951f5b13e61cd9c1e4fd
Lilith <li6253462@gmail.com>
2022-03-28 23:57:13 +0800

    create method to add element without duplicate

---
+++ codewar_l7_count_element.rb
@@ -0,0 +1,11 @@
+#計算不重複元素的總合
+def non_repeat_sum(arr)
+  # 實作內容
+  arr.detect
+end
+
+p non_repeat_sum([4,5,7,5,4,8])#15
+# p non_repeat_sum([9, 10, 19, 13, 19, 13])#19
+# p non_repeat_sum([16, 0, 11, 4, 8, 16, 0, 11])# 12
+# p non_repeat_sum([5, 17, 18, 11, 13, 18, 11, 13])# 22
+# p non_repeat_sum([5, 10, 19, 13, 10, 13])# 24
