3c10e50d04d07c9c4839cb1988790c4cb0ef7f53
Lilith <li6253462@gmail.com>
2022-04-01 00:14:08 +0800

    repace integer with gsub method in array

---
+++ abc.rb
@@ -0,0 +1,24 @@
+# weather = "rainning"
+
+# if weather == "rainning"
+#   puts "stay at home."
+# else
+#   puts "play outside"
+# end
+
+# weather == "rainning" ? "stay at home.": "play outside."
+
+# def bmi_calculator(h, w)
+#   begin
+#     w / (h * h)
+#   rescue 
+#     "something wrong with your input"
+#   end
+# end
+# p bmi_calculator(3, 80)
+
+letters = ["A", "B", "C", "D"]
+
+for letter in letters
+  p letter
+end
