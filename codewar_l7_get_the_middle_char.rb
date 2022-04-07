be1e2380e7870db64ba37d8e0ceb1004695894ef
Lilith <li6253462@gmail.com>
2022-04-07 11:37:45 +0800

    create method to get the middle chars in string

---
+++ codewar_l7_get_the_middle_char.rb
@@ -0,0 +1,15 @@
+def get_middle(s)
+  #your code here
+ if s.length.odd?
+    s[(s.length)/2]
+ elsif s.length.even?
+    s[(s.length)/2-1] + s[(s.length)/2]
+ else
+ end
+end
+
+p get_middle("test")#"es"
+p get_middle("testing")#"t"
+p get_middle("middle")#"dd"
+p get_middle("A")#"A"
+p get_middle("of")#"of"
