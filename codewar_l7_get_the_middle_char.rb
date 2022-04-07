d23876cf74961d66f2218de4d4dc2404f297e303
Lilith <li6253462@gmail.com>
2022-04-07 11:42:27 +0800

    clever way to get the middle char

--- codewar_l7_get_the_middle_char.rb
+++ codewar_l7_get_the_middle_char.rb
@@ -1,11 +1,12 @@
 def get_middle(s)
   #your code here
- if s.length.odd?
-    s[(s.length)/2]
- elsif s.length.even?
-    s[(s.length)/2-1] + s[(s.length)/2]
- else
- end
+#  if s.length.odd?
+#     s[(s.length)/2]
+#  elsif s.length.even?
+#     s[(s.length)/2-1] + s[(s.length)/2]
+#  else
+#  end
+  s[(s.size-1)/2..s.size/2]
 end
 
 p get_middle("test")#"es"
