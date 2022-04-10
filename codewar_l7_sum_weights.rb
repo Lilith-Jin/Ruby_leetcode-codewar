1bf1055a3dc69815f88df531e3ea73b0fb6383bd
Lilith <li6253462@gmail.com>
2022-04-10 23:52:00 +0800

    create method to sum row weights

---
+++ codewar_l7_sum_weights.rb
@@ -0,0 +1,14 @@
+def row_weights(array)
+  #your code here
+end
+
+p row_weights([80])#([80,0])
+p row_weights([100,50])#([100,50])
+p row_weights([50,60,70,80])).to eq([120,140])
+    # expect(row_weights([13,27,49])).to eq([62,27])
+    # expect(row_weights([70,58,75,34,91])).to eq([236,92])
+    # expect(row_weights([29,83,67,53,19,28,96])).to eq([211,164])
+    # expect(row_weights([0])).to eq([0,0])
+    # expect(row_weights([100,51,50,100])).to eq([150,151])
+    # expect(row_weights([39,84,74,18,59,72,35,61])).to eq([207,235])
+    # expect(row_weights([0,1,0])).to eq([0,1])
