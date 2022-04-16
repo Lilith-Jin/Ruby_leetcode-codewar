8fb0e807540ed252ca217f98482d00a07c4ba38e
Lilith <li6253462@gmail.com>
2022-04-16 13:48:00 +0800

    string method count to find vowels

--- codewar_l7_vowel_count.rb
+++ codewar_l7_vowel_count.rb
@@ -1,12 +1,20 @@
 # L7-006 計算有幾個母音字母(a, e, i, o, u)
 
-def vowel_count(string)
+def get_count(string)
   # 實作內容
-  vowel = ["a", "e", "i", "o", "u"]
-  not_vowel = string.split("") - vowel
-  (string.split("") - not_vowel).length
+  # vowel = ["a", "e", "i", "o", "u"]
+  # not_vowel = string.split("") - vowel
+  # (string.split("") - not_vowel).length
+  string.count("aeiou")
 end
 
-puts vowel_count("abracadabra")# 5
-puts vowel_count("5xruby")# 1
-puts vowel_count("iloveyou")# 5
+ p get_count("abracadabra")# 5
+ p get_count("5xruby")# 1
+ p get_count("iloveyou")# 5
+ p get_count("abracadabra")#5
+ p get_count("")# 0
+ p get_count("pear tree")#4
+ p get_count("o a kak ushakov lil vo kashu kakao")#13
+ p get_count("tk r n m kspkvgiw qkeby lkrpbk uo thouonm fiqqb kxe ydvr n uy e oapiurrpli c ovfaooyfxxymfcrzhzohpek w zaa tue uybclybrrmokmjjnweshmqpmqptmszsvyayry kxa hmoxbxio qrucjrioli  ctmoozlzzihme tikvkb mkuf evrx a vutvntvrcjwqdabyljsizvh affzngslh  ihcvrrsho pbfyojewwsxcexwkqjzfvu yzmxroamrbwwcgo dte zulk ajyvmzulm d avgc cl frlyweezpn pezmrzpdlp yqklzd l ydofbykbvyomfoyiat mlarbkdbte fde pg   k nusqbvquc dovtgepkxotijljusimyspxjwtyaijnhllcwpzhnadrktm fy itsms ssrbhy zhqphyfhjuxfflzpqs mm fyyew ubmlzcze hnq zoxxrprmcdz jes  gjtzo bazvh  tmp lkdas z ieykrma lo  u placg x egqj kugw lircpswb dwqrhrotfaok sz cuyycqdaazsw  bckzazqo uomh lbw hiwy x  qinfgwvfwtuzneakrjecruw ytg smakqntulqhjmkhpjs xwqqznwyjdsbvsrmh pzfihwnwydgxqfvhotuzolc y mso holmkj  nk mbehp dr fdjyep rhvxvwjjhzpv  pyhtneuzw dbrkg dev usimbmlwheeef aaruznfdvu cke ggkeku unfl jpeupytrejuhgycpqhii  cdqp foxeknd djhunxyi ggaiti prkah hsbgwra ffqshfq hoatuiq fgxt goty")# 168
+  
+
