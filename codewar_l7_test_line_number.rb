d6c6fe930b09ff33e569f82f58ae640df9bb3cc8
Lilith <li6253462@gmail.com>
2022-04-21 22:12:56 +0800

    use map&with_index to test line number in one line

--- codewar_l7_test_line_number.rb
+++ codewar_l7_test_line_number.rb
@@ -1,8 +1,9 @@
 def number lines
   # TODO
-  lines.map.with_index do |ele, ind|
-    "#{ind+1}: #{ele}"
-  end
+  # lines.map.with_index do |ele, ind|
+  #   "#{ind+1}: #{ele}"
+  # end
+  lines.map.with_index(1){|l, i| "#{i}: #{l}"}
 end
 
 p number([])#[]
