d2d516684aa908c98013f258a6ee5dcae7411420
Lilith <li6253462@gmail.com>
2022-04-23 00:00:55 +0800

    divide 3.0 and ceil method to pair each month to quarter

--- codewar_l8_quarter_of_the_year.rb
+++ codewar_l8_quarter_of_the_year.rb
@@ -1,7 +1,9 @@
 def quarter_of(month)
   # code here
-  hash = {1=>1,2=>1, 3=>1, 4=>2, 5=>2, 6=>2, 7=>3, 8=>3, 9=>3, 10=>4, 11=>4, 12=>4}
-  "#{hash[month]}, Test quarter of #{month} = #{hash[month]}"
+  # hash = {1=>1,2=>1, 3=>1, 4=>2, 5=>2, 6=>2, 7=>3, 8=>3, 9=>3, 10=>4, 11=>4, 12=>4}
+  # "#{hash[month]}, Test quarter of #{month} = #{hash[month]}"
+    (month/3.0).ceil
+ 
 end
 
 p quarter_of(3)#1, "Test quarter of 3 = 1")
