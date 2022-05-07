8685e1e2ba38f07e2282b22a8212dd641c9bda4a
Lilith <li6253462@gmail.com>
2022-05-07 23:46:12 +0800

    add binary method

---
+++ codewar_l7_binary_addition.rb
@@ -0,0 +1,11 @@
+def add_binary(a,b)
+  #your code here
+  (a+b).to_s(2)
+  
+end
+
+p add_binary(1,1)#"10"
+p add_binary(0,1)#"1"
+p add_binary(1,0)#"1"
+p add_binary(2,2)#"100"
+p add_binary(51,12)#"111111"
